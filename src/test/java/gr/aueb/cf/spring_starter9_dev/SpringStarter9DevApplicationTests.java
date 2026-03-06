018b82122bb986b28d72aec6eb25e9d855646c9f
Dim.Roumpekas <jimroumpi@gmail.com>
2026-03-06 15:02:26 +0200

    feat: add view and controller

---
+++ src/test/java/gr/aueb/cf/spring_starter9_dev/SpringStarter9DevApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.aueb.cf.spring_starter9_dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStarter9DevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
