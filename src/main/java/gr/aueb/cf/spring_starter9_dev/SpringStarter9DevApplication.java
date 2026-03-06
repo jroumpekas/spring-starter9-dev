f2d86e64ee04fe6de6f302c2e989b51b4d6ce9af
Dim.Roumpekas <jimroumpi@gmail.com>
2026-03-06 14:57:04 +0200

    initial commit

---
+++ src/main/java/gr/aueb/cf/spring_starter9_dev/SpringStarter9DevApplication.java
@@ -0,0 +1,14 @@
+package gr.aueb.cf.spring_starter9_dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStarter9DevApplication {
+
+	public static void main(String[] args)
+    {
+		SpringApplication.run(SpringStarter9DevApplication.class, args);
+	}
+
+}
